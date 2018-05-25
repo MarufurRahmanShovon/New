8bbbdd3edb34043008658e256e89903eb85eed56
Marufur Rahman <marufurr701@gmail.com>
2018-05-25 15:02:00 +0600

    Add solution for problem `A - New Year Candles` Link: https://codeforces.com/contest/379/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/379/submission/38604469

---
+++ codeforces/379/A.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, div, mod;
+    scanf("%d%d", &a, &b);
+    int sum = a;
+    while(a>=b)
+    {
+        div = (a/b);
+        sum = sum + div;
+        mod = (a%b);
+        a= (div+mod);
+    }
+    printf("%d\n", sum);
+}
+
