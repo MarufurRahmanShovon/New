0aed9c769340bfd9e1f889959676c3c71373d94d
Marufur Rahman <marufurr701@gmail.com>
2018-08-16 17:52:00 +0600

    Add solution for problem `A - Fake NP` Link: https://codeforces.com/contest/805/problem/A Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/805/submission/41651094

---
+++ codeforces/805/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define all v.begin(),v.end()
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll p,q;
+    cin >> p >> q;
+
+    if(p==q) cout << p << endl;
+    else cout << 2 << endl;
+
+    return 0;
+}
