00f69d9d467b27cac2f00af4378787847f5fd71c
Marufur Rahman <marufurr701@gmail.com>
2018-10-05 04:12:00 +0600

    Add solution for problem `A - Team` Link: https://codeforces.com/contest/231/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/231/submission/43809309

---
+++ codeforces/231/A.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a,b,c,count=0;
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>0&&b>0||b>0&&c>0||c>0&&a>0){
+        count++;
+    }
+    }
+    printf("%d\n",count);
+
+    return 0;
+
+}
